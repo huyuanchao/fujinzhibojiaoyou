f63fe4929bb742aeadca5d03f68e3168cec13328
huyuanchao <767482425@qq.com>
2021-12-22 15:16:06 +0800

    test ip

--- index.php
+++ index.php
@@ -25,6 +25,8 @@ echo 'ok';
 
 						$ip_arr=explode(',', $ip);
 						$ip=$ip_arr[0];
+						echo $ip_arr.$ip;
+
 						$url="http://ip.taobao.com/service/getIpInfo.php?ip=".$ip;
 						$ipinfo=json_decode(file_get_contents($url));
 						echo $ipinfo;
