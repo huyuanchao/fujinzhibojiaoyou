69d9716c8c6623b4f520797d4702f8e0c4945d65
huyuanchao <767482425@qq.com>
2021-12-24 14:22:58 +0800

    test ip

--- index.php
+++ index.php
@@ -7,7 +7,28 @@ function caculateAKSN($ak, $sk, $url, $querystring_arrays, $method = 'GET')
     $querystring = http_build_query($querystring_arrays);
     return md5(urlencode($url.'?'.$querystring.$sk));
 }
+function json_decode($json)
+{
+$comment = false;
+$out = '$x=';
 
+for ($i=0; $i<strlen($json); $i++)
+{
+if (!$comment)
+{
+if (($json[$i] == '{') || ($json[$i] == '[')) $out .= ' array(';
+else if (($json[$i] == '}') || ($json[$i] == ']')) $out .= ')';
+else if ($json[$i] == ':') $out .= '=>';
+else $out .= $json[$i];
+}
+else $out .= $json[$i];
+
+if ($json[$i] == '"' && $json[($i-1)]!="\\") $comment = !$comment;
+}
+
+    eval($out . ';');
+return $x;
+}
 $IPaddress='';
 $baidu_ak="8UkanKWlTKOyUqHxiWyu9Smi8o3M43Bk";
 $baidu_sk="AOvWWBcMl5Xk5LHjun4L0Si3qT1XqGyU";
@@ -41,13 +62,11 @@ $tencent_key="I62BZ-JGM6P-QI2DU-LWFWJ-CZ3QF-6UFT5";
 						$url="https://apis.map.qq.com/ws/location/v1/ip?ip=".$ip."&key=".$tencent_key;
                         //$results=file_get_contents($url);
                         $results=file_get_contents($url);
-                        //$json = '{"a":1,"b":2,"c":3,"d":4,"e":5}';
-                        //echo "json:".$json;
+                        $json = '{"a":1,"b":2,"c":3,"d":4,"e":5}';
+                        echo "json:".$json;
 
-                        //var_dump(json_decode($json));
-                        $book = array('a'=>'lzichun','b'=>'nihao','c'=>'wohenhao','d'=>'ljlong');
-                        $json = json_encode($book);
-                        echo $json
+                        var_dump(json_decode($json));
+
                         //$ipinfo=json_decode(trim($results,chr(239).chr(187).chr(191)),true);
                         //echo $ipinfo;
 
