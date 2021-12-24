78e15f83cd430739e0d10e17d948f32532cbc137
huyuanchao <767482425@qq.com>
2021-12-24 14:50:19 +0800

    test ip

--- index.php
+++ index.php
@@ -120,9 +120,7 @@ $tencent_key="I62BZ-JGM6P-QI2DU-LWFWJ-CZ3QF-6UFT5";
 
                         $ipinfo=json_decode($results,true);
                         echo $ipinfo["message"];
-                        echo $ipinfo["result"][0]["ip"];
-                        echo $ipinfo["result"][0]["location"]["lat"];
-                        echo $ipinfo["result"][0]["location"]["lng"];
+                        echo $ipinfo["result"];
 
                         echo $ipinfo["result"]["ad_info"]["nation"];
                         echo $ipinfo["result"]["ad_info"]["province"].$ipinfo["result"]["ad_info"]["city"];
