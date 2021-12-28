9282255d4f58a9dabc1b8256783de6b275519dda
huyuanchao <767482425@qq.com>
2021-12-28 16:47:08 +0800

    test basic.php

--- addons/jy_ppp/inc/mobile/basic.php
+++ addons/jy_ppp/inc/mobile/basic.php
@@ -233,8 +233,7 @@ global $_W,$_GPC;
 				$month=date('m',$member['brith']);
 				$day=date('d',$member['brith']);
 
-				//include $this->template('basic_new');
-				include $this->template('login');
+				include $this->template('basic_new');
 			}
 
 	}
