dc41d3540a35ff704861211a19741b7f36d610c6
huyuanchao <767482425@qq.com>
2021-12-31 12:01:39 +0800

    test.php

--- addons/jy_ppp/inc/mobile/lianxi.php
+++ addons/jy_ppp/inc/mobile/lianxi.php
@@ -127,9 +127,10 @@ global $_W,$_GPC;
 			else
 			{
 				$sitem=pdo_fetch("SELECT * FROM ".tablename('jy_ppp_setting')." WHERE weid=".$weid);
-				$lianxi=pdo_fetchall("SELECT a.id,a.mid as mid,b.avatar,b.nicheng,b.province,b.brith,b.sex,c.height FROM ".tablename('jy_ppp_xinxi')." as a left join ".tablename('jy_ppp_member')." as b on a.mid=b.id left join ".tablename('jy_ppp_basic')." as c on a.mid=c.mid WHERE a.weid=".$weid." AND a.sendid=".$mid." AND a.type!=0 GROUP BY a.mid ORDER BY a.createtime DESC LIMIT 10 ");
+				//$lianxi=pdo_fetchall("SELECT a.id,a.mid as mid,b.avatar,b.nicheng,b.province,b.brith,b.sex,c.height FROM ".tablename('jy_ppp_xinxi')." as a left join ".tablename('jy_ppp_member')." as b on a.mid=b.id left join ".tablename('jy_ppp_basic')." as c on a.mid=c.mid WHERE a.weid=".$weid." AND a.sendid=".$mid." AND a.type!=0 GROUP BY a.mid ORDER BY a.createtime DESC LIMIT 10 ");
+                $lianxi=pdo_fetchall("SELECT a.id,a.mid as mid,b.avatar,b.nicheng,b.province,b.brith,b.sex FROM ".tablename('jy_ppp_xinxi')." as a left join ".tablename('jy_ppp_member')." as b on a.mid=b.id WHERE a.weid=".$weid." AND a.sendid=".$mid." AND a.type!=0 GROUP BY a.mid ORDER BY a.createtime DESC LIMIT 10 ");
 
-				//include $this->template('lianxi');
+				include $this->template('lianxi');
 			}
 
 		}
