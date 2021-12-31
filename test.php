b3260aac00e64829dc6a820a9f7b1a0308386b26
huyuanchao <767482425@qq.com>
2021-12-31 12:22:03 +0800

    test.php

--- test.php
+++ test.php
@@ -1,9 +1,30 @@
 <?php
-echo time();
+function json_decode_defined($json)
+        {
+            $comment = false;
+            $out = '$x=';
+
+            for ($i=0; $i<strlen($json); $i++)
+            {
+                if(!$comment)
+                {
+                    if (($json[$i] == '{') || ($json[$i] == '[')) $out .= ' array(';
+                    else if (($json[$i] == '}') || ($json[$i] == ']')) $out .= ')';
+                    else if ($json[$i] == ':') $out .= '=>';
+                    else $out .= $json[$i];
+                }
+            else $out .= $json[$i];
+
+            if ($json[$i] == '"' && $json[($i-1)]!="\\") $comment = !$comment;
+            }
+
+            eval($out . ';');
+            return $x;
+        }
 
 $data=file_get_contents('index.html');
 echo $data;
 echo 'aaaaaa'
-echo json_decode($data);
+echo json_decode_defined($data);
 echo '....';
 ?>
