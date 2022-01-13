a829e8b615f5dc04d66c23267c4b44c879d58d21
huyuanchao <767482425@qq.com>
2022-01-13 17:17:34 +0800

    sendmsg

--- data/tpl/app/default/jy_ppp/1_login.tpl.php
+++ data/tpl/app/default/jy_ppp/1_login.tpl.php
@@ -1,11 +1,11 @@
-<?php defined('IN_IA') or exit('Access Denied');?><html> 
+<html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <meta content="yes" name="apple-mobile-web-app-capable">
     <meta content="black" name="apple-mobile-web-app-status-bar-style">
     <meta content="telephone=no" name="format-detection">
-    <title><?php  if(!empty($sitem['aname'])) { ?><?php  echo $sitem['aname'];?><?php  } else { ?>靠谱婚恋交友<?php  } ?>  - 靠谱婚恋交友</title>
+    <title>{if !empty($sitem['aname'])}{$sitem['aname']}{else}婚恋交友{/if}  - 婚恋交友</title>
     <link href="../addons/jy_ppp/css/public_reset.css" rel="stylesheet" type="text/css" />
     <link href="../addons/jy_ppp/css/public.css" rel="stylesheet" type="text/css" />
     <link href="../addons/jy_ppp/css/phone_register.css" rel="stylesheet" type="text/css" />
@@ -29,16 +29,16 @@
     <span id="login_btn" class="see_more">立即登录</span>
 	<br>
 
-	
-    <span id="login_onekey" class="see_more">游客浏览 跳过登陆</span>
+    <span id="register_btn" class="see_more register"><a href="{php echo $this->createMobileUrl('zhuce')}">立即注册</a></span>
+    <!--<span id="login_onekey" class="see_more">游客浏览 跳过登陆</span>-->
      <div class="forget">
-       <a href="<?php  echo $this->createMobileUrl('forget')?>"><span>忘记密码？</span></a>
+       <a href="{php echo $this->createMobileUrl('forget')}"><span>忘记密码？</span></a>
     </div>
 </div>
 
 
 <footer class="coverFoot">
-    <!--<p>还没有账号<a href="<?php  echo $this->createMobileUrl('zhuce')?>">立即注册</a></p>-->
+    <!--<p>还没有账号<a href="{php echo $this->createMobileUrl('zhuce')}">立即注册</a></p>-->
 </footer>
 <script src="../addons/jy_ppp/js/zepto.min.js"></script>
 <script src="../addons/jy_ppp/js/waiting.js"></script>
@@ -57,13 +57,13 @@ $(function () {
             return;
         }
         // ajax
-        $.post("<?php  echo $_W['siteroot'].'app/'.substr($this->createMobileUrl('login',array('op'=>'add')),2)?>"+"&mobile="+username+"&password="+password,function(data){
+        $.post("{php echo $_W['siteroot'].'app/'.substr($this->createMobileUrl('login',array('op'=>'add')),2)}"+"&mobile="+username+"&password="+password,function(data){
               if (data == 1) {
-                  window.location.href="<?php  echo $_W['siteroot'].'app/'.substr($this->createMobileUrl('index'),2)?>";
+                  window.location.href="{php echo $_W['siteroot'].'app/'.substr($this->createMobileUrl('index'),2)}";
               }
               else if (data==2)
               {
-                  $.tips("用户名或密码错误!");
+                  $.tips("手机号/昵称或密码错误!");
               }
               else if (data==3)
               {
@@ -73,19 +73,19 @@ $(function () {
                   $.tips("登陆失败！");
               }
           });
-    }) 
+    })
 
 
 
     $("#login_onekey").bind("click",function(){
         // ajax
-        $.post("<?php  echo $_W['siteroot'].'app/'.substr($this->createMobileUrl('login',array('op'=>'onekey')),2)?>",function(data){
+        $.post("{php echo $_W['siteroot'].'app/'.substr($this->createMobileUrl('login',array('op'=>'onekey')),2)}",function(data){
               if (data == 1) {
-                  window.location.href="<?php  echo $_W['siteroot'].'app/'.substr($this->createMobileUrl('index'),2)?>";
+                  window.location.href="{php echo $_W['siteroot'].'app/'.substr($this->createMobileUrl('index'),2)}";
               }
               else if (data==2)
               {
-                  $.tips("用户名或密码错误!");
+                  $.tips("手机号/昵称或密码错误!");
               }
               else if (data==3)
               {
@@ -98,6 +98,6 @@ $(function () {
     })
 });
     </script>
-<script>;</script><script type="text/javascript" src="http://127.0.0.1/app/index.php?i=1&c=utility&a=visit&do=showjs&m=jy_ppp"></script></body>
-<?php (!empty($this) && $this instanceof WeModuleSite) ? (include $this->template('inc/footer', TEMPLATE_INCLUDEPATH)) : (include template('inc/footer', TEMPLATE_INCLUDEPATH));?>
+</body>
+{template 'inc/footer'}
 </html>
